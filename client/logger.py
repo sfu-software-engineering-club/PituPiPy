306d11f7dc0d81180f9dd2b76f06c0443b25f724
jylee2033 <jylee2033@gmail.com>
2023-06-26 21:13:48 +0900

    client messaging stdout to local file

---
+++ client/logger.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+
+class Logger:
+    def __init__(self, location):
+        self.location = location
+
+    def create_log_file(self):
+        self.location = self.location.strip("\"")
+        os.makedirs(os.path.dirname(self.location), exist_ok=True)
+        print(f"Saving log file at : [{self.location}]")
+
+    def log_message(self, message):
+        with open(self.location, 'a') as f:
+            f.write(f"Sending message : [{message}]\n")
